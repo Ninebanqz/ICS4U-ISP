956da8f49a73a52e01c4d4a5f5de438abd5f6c77
Ninebanqz <104600028+Ninebanqz@users.noreply.github.com>
2023-06-13 01:57:45 -0400

    Suit.java

---
+++ Suit.java
@@ -0,0 +1,9 @@
+package com.mycompany.game;
+
+// sets the suit variables and makes it easy to work with between classes
+public enum Suit {
+    Clubs,
+    Diamonds,
+    Spades,
+    Hearts,   
+}
